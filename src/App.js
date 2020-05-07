135c6c4a45934d75869eb09781bf2880c3f9a32a
burnettk <burnettk@users.noreply.github.com>
2020-05-06 22:33:36 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -6,18 +6,91 @@ import React, { useState } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Toast from 'react-bootstrap/Toast';
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import './App.css';
 
 const App = () => (
+     <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <Nav
+  activeKey="/home"
+  onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+>
+  <Nav.Item>
+    <Nav.Link href="/">home</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link href="/about">about</Nav.Link>
+  </Nav.Item>
+</Nav>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+);
+
+function Home() {
+  return (
   <Container className="p-3">
     <Jumbotron className="smart-background App">
       <h1 className="header">Welcome To Smart Kids</h1>
       <Button size="lg">start</Button>
     </Jumbotron>
   </Container>
-);
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
 
 export default App;
 
