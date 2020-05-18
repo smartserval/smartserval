553ffe311bf3a0c158a478e03f91eb1e2da52500
burnettk <burnettk@users.noreply.github.com>
2020-05-17 22:07:22 -0400

    profile page

--- src/App.js
+++ src/App.js
@@ -2,52 +2,49 @@
 // https://upload.wikimedia.org/wikipedia/commons/5/51/Paw-print.svg
 import './App.css';
 
-import { Col, Row } from 'react-bootstrap';
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Jumbotron,
+  Nav,
+  Row
+} from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Nav from 'react-bootstrap/Nav';
 import React from 'react';
 
 import Tank from './Tank.js';
 import paw from './pics/paw.svg';
 
 const App = () => (
-     <Router>
-      <div>
-        <Nav
-  activeKey="/home"
-  onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
->
-  <Nav.Item>
-    <Nav.Link href="/">home</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link href="/about">about</Nav.Link>
-  </Nav.Item>
-</Nav>
+  <Router>
+    <div>
+      <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => console.log(`selected ${selectedKey}`)}
+    >
+        <Nav.Item>
+          <Nav.Link href="/">home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/profile">profile</Nav.Link>
+        </Nav.Item>
+      </Nav>
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/game">
-            <Game />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/game">
+          <Game />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 function Home() {
@@ -64,31 +61,44 @@ function Home() {
   );
 }
 
-function About() {
+function Profile() {
   return (
-    <div>
-      <h2>About</h2>
-    </div>
+    <Container className="p-3 smart-background centered">
+      <h1 className="header outlined">Profile</h1>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Choose your name!</Form.Label>
+          <Form.Control as="select">
+            <option>Ellen</option>
+            <option>Abby</option>
+            <option>David</option>
+            <option>Danny</option>
+            <option>Lila</option>
+          </Form.Control>
+        </Form.Group>
+    </Form>
+    </Container>
   );
 }
 
 function Game() {
   return (
     <Container className="p-3 centered firelands">
-  <Row>
-    <Col>1 of 1</Col>
-    <Col>1 of 1</Col>
-  </Row>
+      <Row>
+        <Col>
+          <Tank automatic={true} />
+        </Col>
+        <Col>
+          <Tank />
+        </Col>
+      </Row>
       <h1 className="header outlined">Firelands</h1>
       <Container>
-        <Tank automatic={true} />
-        <Tank />
 
         <img src={paw} className="paw-button-image" alt="paw" />
       </Container>
     </Container>
   );
 }
-
 
 export default App;
